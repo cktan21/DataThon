6749d6acdeb69004b4c6297b1fff3134f6b55ce4
Kevin Tan <cekevintan@gmail.com>
2025-01-30 23:20:57 +0800

    intial commiut

---
+++ Kevin Stuff/commonly seen datathon techniques.py
@@ -0,0 +1,21 @@
+NLP (Natrual Language Process)
+- How to derive meaning from human languages 
+
+Techniques 
+a. Segmentisation 
+- Split it based on space/comma/puncutation 
+
+Example 
+'I heEARD SUFFERING MAKES one wiser, im about to make you very wise today'
+
+#Tranform 
+['I heEARD SUFFERING MAKES one wiser' , 'im about to make you very wise today']
+
+b. Tokenising 
+- split to its individual words 
+['I', 'heEARD', 'SUFFERING', 'MAKES', 'one', 'wiser' , 'im', 'about', 'to', 'make', 'you', 'very' ,'wise' 'today']
+
+c. Stop words 
+- get rid of non functional words ie and, are, was, in
+
+d. 
